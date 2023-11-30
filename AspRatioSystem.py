e3f4adfc73936af1f8852255259790b19d06f088
Guan-Hao Fu <88991064+3A817060@users.noreply.github.com>
2023-11-30 23:01:10 +0800

    Add files via upload

--- AspRatioSystem.py
+++ AspRatioSystem.py
@@ -5,6 +5,8 @@
 import cv2
 import copy
 import numpy as np
+import Bicubic.bicubic as BC
+import time
 
 class AspRatioSystem():
     def __init__(self, img, t_size) :
@@ -30,7 +32,7 @@ class AspRatioSystem():
         converted_w = int(w * target_ratio)     # Applied to width
         img2 = cv2.resize(img2, (converted_w, target_h), cv2.INTER_AREA)
         self.output  = np.zeros((target_h, target_w, 3), dtype=np.uint8)
-        print(f"img2.shape={img2.shape}, self.output={self.output.shape}")
+        # print(f"img2.shape={img2.shape}, self.output={self.output.shape}")
 
         center_diff_w = int(target_w/2 - converted_w/2)
         self.output[:, center_diff_w:target_w-center_diff_w] = img2[:] # padding 4:3 video on a 16:9 monitor
@@ -42,23 +44,76 @@ class AspRatioSystem():
 
     # Nonlinear Scaling Method
     def NLS(self):
-        img2 = self.input.copy()
-        (h, w) = img2.shape[:2]
+        # --------------------------------
+        # image input & get the size of input image and target image
+        imgc = self.input.copy()
+        (h, w) = imgc.shape[:2]
         (target_h, target_w) = self.t_size  # (height,width) is (1080,1920) on a 16:9 monitor       
 
-        f1 = target_h/h             # zoom factor of the linear area
-        Wm = int(w * f1)                 # the width of the post scaling area M
-        Wl_r = int((target_w - Wm)/2)    # the width of the post scaling area L R
-        # print(f"f1 = {f1}, Wm={Wm}")
-        a1= 0                       # the first element of the equal difference progression
-        d = ((1/f1)-a1)/Wl_r   # the common difference on non-Linear region
+        # --------------------------------
+        # the parameter of NLS function
+        Rm = 0.85                                # the ratio of the width of linear area 
+        Rl_r = int(w*((1-Rm)/2))                # the ratio of the width of nonlinear area 
+        fl = target_h/h                         # upscaling factor of the linear area of Height
+        Wm = int(w * Rm * fl)                   # the width of the post scaling area M
+        Wl_r = int((target_w - Wm)/2)           # the width of the post scaling area L R
+        a1= 0                                   # the first element of the equal difference progression
+        # d = ((1/f1)-a1)/Wl_r                    # the common difference on non-Linear region
+        d = 1/(fl*Wl_r)                          # the common difference on non-Linear region
+        # d *= fl_w
+        print(f"f1 = {fl}, Wm={Wm}, Wl_r={Wl_r}")
 
-        img2 = cv2.resize(img2, (Wm, target_h), cv2.INTER_AREA) # Linear area(M)
+        # --------------------------------
+        # Get 3 seperated region on original image
+        # Padding Region L 
+        imgL = imgc[:, 0:Rl_r]
+        imgL_pa = np.zeros((h+3,Rl_r+3,3))
+        imgL_pa[:-3,:,:] = imgc[:, 0:Rl_r+3, :]
+        # Padding Region M 
+        imgM = imgc[:, Rl_r:w-Rl_r]
+        imgM_pa = np.zeros((h+3,w-(2*Rl_r),3))
+        imgM_pa[:-3,:,:] = imgc[:, Rl_r:w-Rl_r]
+        # Padding Region R
+        imgR = imgc[:, w-Rl_r:]
+        # imgR = cv2.flip(imgR, 1)
+        imgR_pa = np.zeros((h+3,Rl_r+3,3))
+        imgR_pa[:-3,:,:] = imgc[:, w-Rl_r-3:]
+        print(f"imgc.shape={imgc.shape}, imgM.shape={imgM.shape}, imgR_pa.shape={imgR_pa.shape}, imgL_pa.shape={imgL_pa.shape}, distance={d}")
+
+        # --------------------------------
+        # Resize 3 seperated region depend on original image
+        start_time = time.time()
+        # self.plot_result_1(imgL)
+        imgL_r = BC.bicubic(imgL, imgL_pa, d, [target_h, Wl_r], is_reverse=False)                  # Nonlinear area(L)
+        # self.plot_result_1(imgL_r)
+        # imgR_r = cv2.resize(imgR, (Wl_r, target_h), interpolation=cv2.INTER_CUBIC)    # Nonlinear area(L)
+
+        # imgM_r = BC.bicubic(imgM, imgM_pa, d, [target_h, Wm], False,linear=True)    # Linear area(M)
+        imgM_r = cv2.resize(imgM, None, fx=fl, fy=fl, interpolation=cv2.INTER_CUBIC)    # Linear area(M)
+
+        imgR_r = BC.bicubic(imgR, imgR_pa, d, [target_h, Wl_r], is_reverse=True)                  # Nonlinear area(R)
+        # imgR_r = BC.bicubic(imgR, imgR_pa, d, [target_h, Wl_r], True)                            # Nonlinear area(R)
+        # imgR_r = cv2.resize(imgR, (Wl_r, target_h), interpolation=cv2.INTER_CUBIC)    # Nonlinear area(R)
+        end_time = time.time()
+        print(f"imgM_resize.shape={imgM_r.shape}, imgR_resize.shape={imgR_r.shape}")
+        print(f"elapsed_time={end_time - start_time}")
+        # --------------------------------
+        # Pad resized image to the output empty frame
         self.output  = np.zeros((target_h, target_w, 3), dtype=np.uint8)    # Black area which image size is (1920,1080)
-        print(f"img2.shape={img2.shape}, distance between adjacent pixels={d}, the width of the post scaling area L={Wl_r}")
+        center_diff_w = int(target_w/2 - imgM_r.shape[1]/2)
+        self.output[:, 0:Wl_r] = imgL_r[:]                                  # padding Nonlinear area(R) on a 16:9 monitor
+        self.output[:, center_diff_w:target_w-center_diff_w] = imgM_r[:]    # padding Linear area(M) on a 16:9 monitor
+        self.output[:, Wl_r+Wm:] = imgR_r[:]                                  # padding Nonlinear area(L) on a 16:9 monitor
+        # print(f"imgc.shape={imgc.shape}, imgM.shape={imgM.shape}, distance between adjacent pixels={d}, the width of the post scaling area L={Wl_r}")
 
     def plot_result(self):
         cv2.namedWindow("demo", cv2.WINDOW_NORMAL)
         cv2.imshow("demo", self.output)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+    def plot_result_1(self, img):
+        cv2.namedWindow("demo", cv2.WINDOW_NORMAL)
+        cv2.imshow("demo", img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ img = cv2.imread("./Dataset.jpg", 1) # 4:3 video frame;size=800x600
 target_size = (1080, 1920)  # 16:9 video frame;size=1920x1080
 Pillarbox = ARS(img=img, t_size=target_size)
 Pillarbox.NLS()
+Pillarbox.plot_result()
 # Pillarbox.Resize()
 # Pillarbox.plot_result()
 # Pillarbox.Pillarboxing()
